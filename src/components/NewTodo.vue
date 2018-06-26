b701be5eb75397d87be615aefc8243d789414b8d
sakamoto.m <sakamoto.m@water-cell.jp>
2018-06-26 09:49:49 +0900

    newtodo

---
+++ src/components/NewTodo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input
+        type="text"
+        v-model="text"
+        class="new"
+        @keyup.enter="submit" />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "NewTodo",
+    data () {
+      return {
+        text: "",
+      }
+    },
+    methods: {
+      submit () {
+        this.$emit("createTodo", this.text)
+      },
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
